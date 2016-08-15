7efac4f87aa4915b64aa9044d64d4e1641f53c51
Rohit Jain <rohit.jain@shadowfax.in>
2016-08-15 16:42:17 +0530

    added scriptt parse raw csv and set data in redis

--- nextMail/data_parse.py
+++ nextMail/data_parse.py
@@ -1,5 +1,6 @@
 import csv
 import pdb
+from redis_util import RedisHelper
 
 f = open('raw_data_bank.csv', 'r')
 
@@ -10,22 +11,30 @@ f = open('raw_data_bank.csv', 'r')
     
 """
 bank_details = {}
+bank_city_mapping = {}
 
 try:
-    reader = csv.reader(f)
-    #Pop metadata - no use
+    reader = csv.reader(f);
+    #Pop metadata 
     #ifsc,bank_id,branch,address,city,district,state,bank_name
-    reader.pop(0)
+    reader.next()
 
     for row in reader:
         ifsc, bank_id, branch, address, city, district, state ,bank_name = row[0], row[1], row[2], row[3],row[4], row[5], row[6], row[7]
         if bank_name not in bank_details:
             bank_details[bank_name] = {}
+            bank_city_mapping[bank_name] = []
+
         if city not in bank_details[bank_name]:
             bank_details[bank_name][city] = []
+            bank_city_mapping[bank_name].append(city)
+        
         bank_details[bank_name][city].append({"ifsc":ifsc, "bank_id": bank_id, "branch": branch, "address":address, 
                                             "district":district, "state":state })
 finally:
     f.close()
 
-pdb.set_trace()
+
+for bank_name in bank_details.keys():
+    RedisHelper().set_redis_hash(bank_name, bank_details[bank_name])
+    RedisHelper().set_redis_list(bank_name +'_city', bank_city_mapping[bank_name])
